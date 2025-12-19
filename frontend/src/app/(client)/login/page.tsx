5550f0fcf42b182df726feecb38a85d7a44d683f
Munkhuu0000 <25LP8703@pinecones-iMac.local>
2025-12-19 12:22:57 +0800

    change111

--- frontend/src/app/(client)/login/page.tsx
+++ frontend/src/app/(client)/login/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { createContext, Dispatch, SetStateAction, useState } from "react";
 import { CreateAccount } from "../../_components/auth/CreateAccount";
 import { Createpassword } from "../../_components/auth/CreatePassword";
 import { CreateStrongPassword } from "../../_components/auth/CreateStrongPassword";
@@ -7,15 +8,23 @@ import { Login } from "../../_components/auth/Login";
 import { Resetpassword } from "../../_components/auth/Resetpassword";
 import { VerifyEmail } from "../../_components/auth/VerifyEmail";
 
+type StepContextType = {
+  setStep: Dispatch<SetStateAction<StepContextType>>;
+  step: number;
+};
+export const stepContext = createContext<StepContextType>(
+  {} as StepContextType
+);
+
 export default function login() {
+  const [step, setStep] = useState<StepContextType>();
+
   return (
     <div className="flex gap-4 flex-col">
-      <Login />
-      <Resetpassword />
-      <VerifyEmail />
-      <Createpassword />
-      <CreateAccount />
-      <CreateStrongPassword />
+      {step === 1 && <Login />}
+      {step === 2 && <Resetpassword />}
+      {step === 3 && <VerifyEmail />}
+      {step === 4 && <Createpassword />}
     </div>
   );
 }
--- frontend/src/app/(client)/signup/page.tsx
+++ frontend/src/app/(client)/signup/page.tsx
@@ -1,19 +1,11 @@
 "use client";
 
 import { CreateAccount } from "../../_components/auth/CreateAccount";
-import { Createpassword } from "../../_components/auth/CreatePassword";
 import { CreateStrongPassword } from "../../_components/auth/CreateStrongPassword";
-import { Login } from "../../_components/auth/Login";
-import { Resetpassword } from "../../_components/auth/Resetpassword";
-import { VerifyEmail } from "../../_components/auth/VerifyEmail";
 
 export default function login() {
   return (
     <div className="flex gap-4 flex-col">
-      <Login />
-      <Resetpassword />
-      <VerifyEmail />
-      <Createpassword />
       <CreateAccount />
       <CreateStrongPassword />
     </div>
