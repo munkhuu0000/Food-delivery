990698811a310119152a245f9c81b873ed55e995
Munkhuu0000 <25LP8703@pinecones-iMac.local>
2025-12-19 11:09:25 +0800

    change 110

--- frontend/src/app/(client)/_components/(Header component)/AddCartButton.tsx
+++ frontend/src/app/(client)/_components/(Header component)/AddCartButton.tsx
@@ -1,14 +1,5 @@
 "use client";
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "@/components/ui/drawer";
+
 import { Input } from "@/components/ui/input";
 import { ChevronRight, MapPin, ShoppingCart, User } from "lucide-react";
 import { Button } from "@/components/ui/button";
@@ -24,51 +15,38 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export const AddCartButton = () => {
   return (
-    <div className="rounded-full">
-      <Drawer>
-        <DrawerTrigger>Open</DrawerTrigger>
-        <DrawerContent side="right">
-          <DrawerHeader>
-            <DrawerTitle>Are you absolutely sure?</DrawerTitle>
-            <DrawerDescription>This action cannot be undone.</DrawerDescription>
-          </DrawerHeader>
-          <DrawerFooter>
-            <Button>Submit</Button>
-            <DrawerClose>
-              <div>Cancel</div>
-            </DrawerClose>
-          </DrawerFooter>
-        </DrawerContent>
-      </Drawer>
-      {/* <Sheet>
-          <SheetTrigger>
-            <ShoppingCart />
-          </SheetTrigger>
-          <SheetContent className="bg-[#18181B33] w-min-[535px] h-screen opacity-100">
-            <SheetHeader>
-              <SheetTitle className=" flex flex-row gap-3"></SheetTitle>
-
-              <SheetDescription className="w-full h-full">
-                <>
-                  <ShoppingCart /> Order detail
-                </>
-                <Tabs defaultValue="Cart" className="w-full h-full">
-                  <TabsList className="w-full h-full">
-                    <TabsTrigger
-                      value="Cart"
-                      className="text-[18px] font-normal rounded-5"
-                    >
-                      Cart
-                    </TabsTrigger>
-                    <TabsTrigger value="Order">Order</TabsTrigger>
-                  </TabsList>
-                  <TabsContent value="Cart">Foods</TabsContent>
-                  <TabsContent value="Order">More Foods</TabsContent>
-                </Tabs>
-              </SheetDescription>
-            </SheetHeader>
-          </SheetContent>
-        </Sheet> */}
+    <div className="rounded-full bg-[#F4F4F5] w-9 h-9 flex justify-center items-center">
+      <Sheet>
+        <SheetTrigger>
+          <ShoppingCart />
+        </SheetTrigger>
+        <SheetContent className="bg-[#18181B33] w-min-[535px] h-screen ">
+          <SheetHeader>
+            <SheetTitle className=" flex flex-row gap-3 ">
+              <>
+                <ShoppingCart className="w-6 h-6 text-[#FAFAFA]" />
+                <p className="text-[20px] font-semibold text-[#FAFAFA]">
+                  Order detail
+                </p>
+              </>
+            </SheetTitle>
+            <SheetDescription className="w-full h-full">
+              <Tabs defaultValue="Cart" className="w-full h-11">
+                <TabsList className="w-full h-11 rounded-full">
+                  <TabsTrigger value="Cart" className=" rounded-full h-9">
+                    <p className="text-[18px] font-normal">Cart</p>
+                  </TabsTrigger>
+                  <TabsTrigger value="Order" className=" rounded-full h-9">
+                    <p className="text-[18px] font-normal">Order</p>
+                  </TabsTrigger>
+                </TabsList>
+                <TabsContent value="Cart"></TabsContent>
+                <TabsContent value="Order"></TabsContent>
+              </Tabs>
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
     </div>
   );
 };
